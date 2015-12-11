c25e732f449f79367072242962e8dbd1ee084418
irene robb & Aaron Robb <pair+2irenerobb72.aaronmrobb@devbootcamp.com>
2015-12-11 14:58:45 +1300

    tests fixed

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const loadDb = require('./helpers/load-db')
-const app = require('../index')
+const app = require('../app')
 const request = require('co-supertest').agent(app.listen())
 const expect = require('chai').expect
 require ('co-mocha')
@@ -12,29 +12,27 @@ describe('API catpoints', () => {
     it('returns 200', function *() {
       yield request.get('/cats').expect(200).end()
     })
-    xit('returns all cats', function *(){
+    it('returns all cats', function *(){
       let res = yield request.get('/cats').end()
       expect(res.body.length).to.equal(5)
     })
   })
 
   describe('GET /cats/:id', () => {
-    before(() => ('cats') )
-    xit('returns 200 if found', function *(){
+    it('returns 200 if found', function *(){
       yield request.get('/cats/1').expect(200).end()
     })
-    xit('returns cat with correct id', function *(){
+    it('returns cat with correct id', function *(){
       let res = yield request.get('/cats/1').end()
       expect(res.body.id).to.equal(1)
     })
   })
 
   describe('POST /cats', () => {
-    beforeEach( () => ('cats'))
-    xit('returns 201 if move was successful', function *(){
+    it('returns 201 if move was successful', function *(){
       yield request.post('/cats').expect(201).end()
     })
-    xit('last cat in database is equal to cat posted', function *(){
+    it('last cat in database is equal to cat posted', function *(){
       yield request.post('/cats').expect(201).end()
       let res = yield request.get('/cats').end()
       expect(res.body.length).to.equal(6)
@@ -42,20 +40,18 @@ describe('API catpoints', () => {
   })
 
   describe('PATCH /cats/:id', () => {
-    before( () => ('cats'))
-    xit('returns 200 if patched', function *(){
+    it('returns 200 if patched', function *(){
       yield request.patch('/cats/1').expect(200).end()
     })
-    xit('returns cat with new property', function *(){
+    it('returns cat with new property', function *(){
     })
   })
 
   describe('DELETE /cats', () => {
-    beforeEach( () => ('moves'))
-    xit('returns 204 if deleted', function *(){
+    it('returns 204 if deleted', function *(){
       yield request.del('/cats/1').expect(204).end()
     })
-    xit('returns 404 if cat is deleted', function *(){
+    it('returns 404 if cat is deleted', function *(){
       yield request.del('/cats/1').expect(404).end()
       let res = yield request.get('/cats').end()
       expect(res.body.length).to.equal(4)
