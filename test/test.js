02cff7de47e51be470ef0ab1966ae0e28e18457e
irene robb & Aaron Robb <pair+2irenerobb72.aaronmrobb@devbootcamp.com>
2015-12-11 14:32:25 +1300

    tests need more work

--- test/test.js
+++ test/test.js
@@ -9,31 +9,42 @@ describe('API catpoints', () => {
 
   describe('GET /cats', () => {
     before(() => ('cats') )
-    xit('returns 200', function *(){
+    xit('returns 200', function *() {
+      yield request.get('/cats').expect(200).end()
     })
     xit('returns all cats', function *(){
+      let res = yield request.get('/cats').end()
+      expect(res.body.length).to.equal(5)
     })
   })
 
   describe('GET /cats/:id', () => {
     before(() => ('cats') )
     xit('returns 200 if found', function *(){
+      yield request.get('/cats/1').expect(200).end()
     })
     xit('returns cat with correct id', function *(){
+      let res = yield request.get('/cats/1').end()
+      expect(res.body.id).to.equal(1)
     })
   })
 
   describe('POST /cats', () => {
     beforeEach( () => ('cats'))
     xit('returns 201 if move was successful', function *(){
+      yield request.post('/cats').expect(201).end()
     })
     xit('last cat in database is equal to cat posted', function *(){
+      yield request.post('/cats').expect(201).end()
+      let res = yield request.get('/cats').end()
+      expect(res.body.length).to.equal(6)
     })
   })
 
   describe('PATCH /cats/:id', () => {
     before( () => ('cats'))
     xit('returns 200 if patched', function *(){
+      yield request.patch('/cats/1').expect(200).end()
     })
     xit('returns cat with new property', function *(){
     })
@@ -42,8 +53,12 @@ describe('API catpoints', () => {
   describe('DELETE /cats', () => {
     beforeEach( () => ('moves'))
     xit('returns 204 if deleted', function *(){
+      yield request.del('/cats/1').expect(204).end()
     })
     xit('returns 404 if cat is deleted', function *(){
+      yield request.del('/cats/1').expect(404).end()
+      let res = yield request.get('/cats').end()
+      expect(res.body.length).to.equal(4)
     })
   })
 })
